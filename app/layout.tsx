c869bc1b8dd20a3fb592ba00a6921dbcacf6adfd
m.razaghi <m.razzaghi201@gmail.com>
2025-08-02 12:46:07 +0330

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,49 @@
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
+import { ThemeProvider } from 'next-themes'
+
+const geistSans = Geist({
+  subsets: ['latin'],
+  variable: '--font-geist-sans',
+  display: 'swap',
+  adjustFontFallback: false,
+})
+
+const geistMono = Geist_Mono({
+  subsets: ['latin'],
+  variable: '--font-geist-mono',
+  display: 'swap',
+  adjustFontFallback: false,
+})
+
+export const metadata: Metadata = {
+  title: 'React/Shadcn Calendar',
+  description: 'By @charlietlamb',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={`${geistSans.variable} ${geistMono.variable} `}
+    >
+      <head />
+      <body className="antialiased font-sans">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
